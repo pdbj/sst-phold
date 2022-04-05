3aa7087712ca661d965896f19aac9f58886476b9
Peter D. Barnes, Jr <barnes26@llnl.gov>
2022-04-05 11:15:27 -0700

    Update batch script to support multiple nodes, ranks per node

--- batch.phold.sh
+++ batch.phold.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-srun -N 1 -n 2 sst  --print-timing-info tests/phold.py -- -s 100 -n 1024 -e 128
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=2
+#SBATCH --job-name=phold
+#SBATCH --partition=pdebug
+#SBATCH --account=wireless
+
+date
+pwd
+which sst
+
+srun --nodes=2 --ntasks=2 sst --num_threads=2 --print-timing-info tests/phold.py -- --number 512 --events 1024 --stop 1000
+
 
