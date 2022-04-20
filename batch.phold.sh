3bf704605b4f471a2e5ed7d04e8fc4bcb5010289
Peter D. Barnes, Jr <barnes26@llnl.gov>
2022-04-20 12:34:18 -0700

    Minor arg changes

--- batch.phold.sh
+++ batch.phold.sh
@@ -4,13 +4,17 @@
 #SBATCH --ntasks-per-node=2
 #SBATCH --job-name=phold
 #SBATCH --partition=pdebug
-#SBATCH --account=wireless
+#SBATCH --account=dance
 
 date
 echo "PWD:" `pwd`
 echo "SST:" `which sst`
 
-srun --nodes=2 --ntasks=2 sst --num_threads=2 --print-timing-info tests/phold.py -- --number 512 --events 1024 --stop 1000
+srun --nodes=2 --ntasks=2 sst --num_threads=2 tests/phold.py -- \
+--number 512 --events 1024 --stop 1000 --buffer 1000
+
+# --number 512 --events 1024 --stop 1000
+# --number 4 --events 1 --stop 100 --pverbose --buffer 1000
 
 echo
 date
