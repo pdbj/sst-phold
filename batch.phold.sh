3eff1004dbb118293e22d7374b3c994e10373e93
Peter D. Barnes, Jr <barnes26@llnl.gov>
2022-05-11 17:58:52 -0700

    Use just one node with multiple ranks

--- batch.phold.sh
+++ batch.phold.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --job-name=phold
 #SBATCH --partition=pdebug
@@ -10,8 +10,10 @@ date
 echo "PWD:" `pwd`
 echo "SST:" `which sst`
 
-srun --nodes=2 --ntasks=2 sst --num_threads=2 tests/phold.py -- \
---number 512 --events 1024 --stop 1000 --buffer 1000
+srun --nodes=1 --ntasks=2 \
+     sst --num_threads=2 \
+     tests/phold.py -- \
+     --number 512 --events 1024 --stop 1000 --buffer 1000 --thread 0.5
 
 # --number 512 --events 1024 --stop 1000
 # --number 4 --events 1 --stop 100 --pverbose --buffer 1000
